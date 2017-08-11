323d1f6bbc07929bbcd7eefd88e9337ed95e73a6
Sergey Yushmanov <s.yushmanov777@gmail.com>
2017-08-11 23:01:52 +0700

    gulp config

---
+++ gulpfile.babel.js
@@ -0,0 +1,39 @@
+import gulp from 'gulp';
+import pug from 'gulp-pug';
+import stylus from 'gulp-stylus';
+import autoprefixer from 'gulp-autoprefixer';
+import babel from 'gulp-babel';
+import cleanCss from 'gulp-clean-css';
+import imagemin from 'gulp-imagemin';
+import watch from 'gulp-watch';
+
+gulp.task('pug', () =>
+  gulp.src('./src/pug/**/!(_)*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('./dest')))
+
+gulp.task('js', () =>
+  gulp.src('./src/js/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('./dest/js')))
+
+gulp.task('stylus', () =>
+  gulp.src('./src/stylus/style.styl')
+    .pipe(stylus())
+    .pipe(autoprefixer())
+    .pipe(cleanCss())
+    .pipe(gulp.dest('./dest/css')))
+
+gulp.task('img', () =>
+  gulp.src('./src/images/**/*.*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dest/img')))
+
+gulp.task('watch', () => {
+  watch('./src/stylus/**/*.styl', _=>gulp.start('stylus'));
+  watch('./src/pug/**/*.pug', _=>gulp.start('pug'));
+  watch('./src/images/**/*.*', _=>gulp.start('img'));
+  watch('./src/js/**/*.*', _=>gulp.start('js'));
+});
+
+gulp.task('default', ['img', 'stylus', 'pug', 'js', 'watch'])
